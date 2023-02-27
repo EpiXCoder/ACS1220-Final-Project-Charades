883b31b728e109a97a9ab4b2520e9ab8690f8b18
Vithusha Ravirajan <vithusharavirajan@Vithus-MBP.local>
2023-02-26 22:39:26 -0500

    deleted irrelevant models

--- charades_app/models.py
+++ charades_app/models.py
@@ -10,12 +10,9 @@ class Word(db.Model):
   name = db.Column(db.String(100), nullable=False)
   hint = db.Column(db.String(300), nullable=False)
 
-  # The category the word belongs to
   category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
-  # category_name = db.Column(db.String(100), nullable=False)
   category = db.relationship('Category', back_populates = 'word')
-    # category = db.relationship('Category', secondary='word_category', back_populates = 'word')
-  # 
+ 
   def __str__(self):
       return f'<Word: {self.name}>'
 
@@ -27,35 +24,13 @@ class Category(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(100), nullable=False, unique=True)
   word = db.relationship('Word', back_populates = 'category')
-    # word = db.relationship('Word', secondary='word_category', back_populates = 'category')
+
 
   def __str__(self):
       return f'{self.name}'
 
   def __repr__(self):
       return f'Category(name={self.name})'
-
-# word_category_table = db.Table('word_category',
-#   db.Column('word_id', db.Integer, db.ForeignKey('word.id')),
-#   db.Column('category_id', db.Integer, db.ForeignKey('category.id'))
-# )
-
-class Gameinstance(db.Model):
-  """Game Instance model"""
-  id = db.Column(db.Integer, primary_key=True)
-  # user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-  # user = db.relationship('User', secondary='user_gameinstance')
-  category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
-  category = db.Column(db.String, db.ForeignKey('category.name'), nullable=False)
-  # word_array = db.Column(ARRAY(db.String), nullable = False)
-
-  def __repr__(self):
-      return f'<GameInstance: {self.id}>'
-
-# user_gameinstance_table = db.Table('user_gameinstance',
-#   db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
-#   db.Column('gameinstance_id', db.Integer, db.ForeignKey('gameinstance.id')),
-# )
 
 
 class User(UserMixin, db.Model):
@@ -65,8 +40,6 @@ class User(UserMixin, db.Model):
   firstname = db.Column(db.String(80), nullable=False)
   lastname = db.Column(db.String(80), nullable=False)
   email = db.Column(db.String(100), nullable=False)
-  # gameinstance_id = db.Column(db.Integer, db.ForeignKey('gameinstance.id'), nullable=False)
-  # gameinstance = db.relationship('Gameinstance', secondary='user_gameinstance')
 
   def __repr__(self):
       return f'<User: {self.id}>'
